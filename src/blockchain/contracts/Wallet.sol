f7ebe4e12eb2d77d3e00640476c21f4652a3e3e2
govinda777 <govinda777@gmail.com>
2023-06-28 13:25:06 -0300

    up

--- src/blockchain/contracts/Wallet.sol
+++ src/blockchain/contracts/Wallet.sol
@@ -1,11 +1,10 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-import "./AccessManager.sol";
+import "@openzeppelin/contracts/token/ERC721/IERC721Receiver.sol";
 
-contract Wallet is AccessManager {
-    
-    constructor() {
-        _setupRole(DEFAULT_ADMIN_ROLE, msg.sender);
+contract Wallet is IERC721Receiver {
+    function onERC721Received(address, address, uint256, bytes calldata) external pure override returns(bytes4) {
+        return this.onERC721Received.selector;
     }
 }
