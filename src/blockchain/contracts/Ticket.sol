c1c43dc40a345ccba0c5df1e0f0ccd0df8116dc5
govinda777 <govinda777@gmail.com>
2023-06-28 10:11:56 -0300

    up

--- src/blockchain/contracts/Ticket.sol
+++ src/blockchain/contracts/Ticket.sol
@@ -3,10 +3,12 @@ pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "./AccessManager.sol"; // Import the AccessManager contract
 
 contract Ticket is ERC721URIStorage {
     uint256 public nextTokenId;
     address public admin;
+    AccessManager public accessManager; // Create an instance of AccessManager
 
     struct Event {
         string name;
@@ -18,8 +20,9 @@ contract Ticket is ERC721URIStorage {
 
     Event[] public events;
 
-    constructor() ERC721("EventTicket", "ETKT") {
+    constructor(address accessManagerAddress) ERC721("EventTicket", "ETKT") {
         admin = msg.sender;
+        accessManager = AccessManager(accessManagerAddress); // Initialize the AccessManager instance
     }
 
     function createEvent(
@@ -29,21 +32,29 @@ contract Ticket is ERC721URIStorage {
         uint256 ticketPrice,
         uint256 totalTickets
     ) external {
-        require(msg.sender == admin, "only admin");
+        require(
+            accessManager.hasRole(accessManager.ORGANIZER_ROLE(), msg.sender),
+            "only organizer"
+        ); // Check if the sender has the ORGANIZER_ROLE
         events.push(Event(name, location, date, ticketPrice, totalTickets));
     }
 
     function mint(address to, uint256 eventId) public {
-        require(msg.sender == admin, "only admin");
+        require(
+            accessManager.hasRole(accessManager.ORGANIZER_ROLE(), msg.sender),
+            "only organizer"
+        ); // Check if the sender has the ORGANIZER_ROLE
         require(eventId < events.length, "event does not exist");
-
         _safeMint(to, nextTokenId);
         _setTokenURI(nextTokenId, "tokenURI");
         nextTokenId++;
     }
 
     function burn(uint256 tokenId) external {
-        require(_isApprovedOrOwner(_msgSender(), tokenId), "caller is not owner nor approved");
+        require(
+            _isApprovedOrOwner(_msgSender(), tokenId),
+            "caller is not owner nor approved"
+        );
         _burn(tokenId);
     }
 }
